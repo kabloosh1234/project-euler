68365026010e00a23146caa860a838c270ed7ad2
Ken <kenlin7654@gmail.com>
2022-08-06 17:01:01 -0600

    sdfghj

---
+++ id10.py
@@ -0,0 +1,14 @@
+primes =[]
+num = 2
+
+def is_prime(n):
+  for i in range(2,n):
+    if (n%i) == 0:
+      return False
+  return True
+
+while len(primes) != 2000000 :
+    if is_prime(num) == True:
+        primes.append(num)
+    num = num + 1
+print(sum(primes))
